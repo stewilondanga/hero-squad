09104d1ebee69df2a49fbd949f33b3ec5eac124f
Stewart ilondanga <stilondanga@gmail.com>
2017-12-08 17:39:47 +0300

    add src/main/java/App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -43,6 +43,16 @@ public class App {
     }, new VelocityTemplateEngine());
 
 //This route will be executed when someone clicks a link to see a particular Hero's detail page
+    get("heroes/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+      model.put("hero", hero);
+      model.put("template", "templates/hero.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+//route responsible for rendering the template with the new-team form
 
   }
 }
