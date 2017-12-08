b4cec113ec851f018f8839250b14099f21954036
Stewart ilondanga <stilondanga@gmail.com>
2017-12-08 17:44:52 +0300

    add src/main/java/App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -53,6 +53,12 @@ public class App {
 
 
 //route responsible for rendering the template with the new-team form
+    get("/teams/new", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/teamform.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
 
   }
 }
